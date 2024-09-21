351e66a38e6422bbd5a443b539051300b354f412
ingjuanfg <juan.isfg@gmail.com>
2024-09-21 09:40:07 -0500

    Pruebas Unitarias

---
+++ src/main/java/com/ingjuanfg/Calculadora.java
@@ -0,0 +1,13 @@
+package com.ingjuanfg;
+
+public class Calculadora {
+    private int number = 2;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
